a13d92c3dea7e50cbbed5e555a96025baf3b98cf
tarnas14 <tarnas14@gmail.com>
2020-10-25 23:37:45 +0100

    added a blog post layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images')
 
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return dateObj.toLocaleDateString(undefined, {year: 'numeric', month: 'long', day: 'numeric'});
+  });
+
+  eleventyConfig.addFilter("htmlDateISOString", (dateObj) => {
+    return dateObj.toISOString();
+  });
+
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
     passthroughFileCopy: true,
